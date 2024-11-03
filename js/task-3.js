f0bb8db4b0308445e99343135255916151c44d60
Tatyana <tatyanakorabelnikovamama@gmail.com>
2024-11-03 16:31:10 +0200

    add initial files

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,22 @@
-function getElementWidth(content, padding, border) {
-   const contentWidth = parseFloat(content);
-   const paddingWidth = parseFloat(padding) * 2; // Подвоюємо для обох боків
-   const borderWidth = parseFloat(border) * 2;   // Подвоюємо для обох боків
-   const totalWidth = contentWidth + paddingWidth + borderWidth;
-    return totalWidth; 
+
+function checkForSpam(message) {
+  // Переводимо рядок у нижній регістр, щоб перевірка не залежала від регістру
+  const lowerCaseMessage = message.toLowerCase();
+
+  // Перевіряємо, чи містить рядок слова "spam" або "sale"
+  return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+function checkForSpam(message) {
+  // Переводимо рядок у нижній регістр, щоб перевірка не залежала від регістру
+  const lowerCaseMessage = message.toLowerCase();
+
+  // Перевіряємо, чи містить рядок слова "spam" або "sale"
+  return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
+}
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
